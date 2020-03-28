326d06d679462c17dbee07f82c7bf88eaa8fafb1
Arthur <arthur.bruniau@gmail.com>
2020-03-28 18:36:49 +0100

    Starting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AccueilComponent} from './routes/accueil/accueil.component';
+import {LoginComponent} from './routes/login/login.component';
+import {RegisterComponent} from './routes/register/register.component';
+import {PageNotFoundComponent} from './routes/page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AccueilComponent
+  },
+  {
+    path: 'connexion',
+    component: LoginComponent
+  },
+  {
+    path : 'enregistrement',
+    component: RegisterComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './routes/login/login.component';
+import { RegisterComponent } from './routes/register/register.component';
+import { PageNotFoundComponent } from './routes/page-not-found/page-not-found.component';
+import { AccueilComponent } from './routes/accueil/accueil.component';
+import { NavbarComponent } from './components/core/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    PageNotFoundComponent,
+    AccueilComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
